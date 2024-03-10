c3c27949cfd608b6a6a9adcd1ce5452652d0adb7
JCKalt <ckaltwasser@reminingcorp.com>
2024-03-09 19:15:36 -0500

    mods to default

--- scripts/mods/put-mods.sh
+++ scripts/mods/put-mods.sh
@@ -3,3 +3,5 @@ cp LTSHome.aspx ../../www.annarborfsc.org/Programs/LTS/
 cp Information.aspx ../../www.annarborfsc.org/Membership/
 cp ContractIce.aspx ../../www.annarborfsc.org/Programs/Ice/ContractIce.aspx
 cp blue.css ../../figureskating.blob.core.windows.net/assets/css/themes
+cp default.aspx ../../www.annarborfsc.org/
+
