fe3ba18d05ed550951735f9265f4658531d85a2e
JCKalt <ckaltwasser@reminingcorp.com>
2024-03-09 19:24:13 -0500

    mods

--- scripts/mods/put-mods.sh
+++ scripts/mods/put-mods.sh
@@ -4,4 +4,5 @@ cp Information.aspx ../../www.annarborfsc.org/Membership/
 cp ContractIce.aspx ../../www.annarborfsc.org/Programs/Ice/ContractIce.aspx
 cp blue.css ../../figureskating.blob.core.windows.net/assets/css/themes
 cp default.aspx ../../www.annarborfsc.org/
+cp index.html ../../www.annarborfsc.org/
 
